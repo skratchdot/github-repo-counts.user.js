1425186d281ad42b17c0ba584043c3e21ad4eec7
skratchdot <git@skratchdot.com>
2015-09-01 14:59:58 -0400

    please use github-enhancement-suite instead

--- github-repo-counts.user.js
+++ github-repo-counts.user.js
@@ -4,28 +4,13 @@
 // @description    A user script to display repo counts when browsing Github repository pages.
 // @include        https://github.com/*
 // @match          https://github.com/*
-// @require        https://gist.github.com/skratchdot/5604120/raw/_init.js
-// @require        https://gist.github.com/skratchdot/5604120/raw/repo-counts.js
 // @run-at         document-end
 // @grant          none
 // @icon           http://skratchdot.com/favicon.ico
 // @downloadURL    https://github.com/skratchdot/github-repo-counts.user.js/raw/master/github-repo-counts.user.js
 // @updateURL      https://github.com/skratchdot/github-repo-counts.user.js/raw/master/github-repo-counts.user.js
-// @version        1.9
+// @version        2.0
 // ==/UserScript==
-/*global SKRATCHDOT, document */
 
-// This code is only going to run for browsers that don't support
-// the @require annotation when executing userscripts.
-if ('undefined' === typeof SKRATCHDOT) {
-	var addScript = function (src) {
-		'use strict';
-		var script = document.createElement('script');
-		script.src = src;
-		document.body.appendChild(script);
-		document.body.removeChild(script);
-	};
-
-	// Required by: repo-filter-info
-	addScript('https://gist.github.com/skratchdot/5604120/raw/repo-counts.js');
-}
+console.log('This script has been updated and is part of the Github Enhancement Suite' +
+		' found at: https://github.com/skratchdot/github-enhancement-suite');
